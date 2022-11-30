2527ca5c8e62c687517da56d81308430eca3a8ee
roshanthamalaka <roshanthamalaka@gmail.com>
2022-11-30 23:19:23 +0530

    Added Changes to sg rules

--- sg.tf
+++ sg.tf
@@ -11,6 +11,15 @@ resource "aws_security_group" "websg" {
     cidr_blocks      = ["0.0.0.0/0"]
     #ipv6_cidr_blocks = "0.0.0.0/0"
   }
+  #Added After Initial Commit
+  ingress {
+    description      = "SSH Allow"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = "0.0.0.0/0"
+  }
 
   egress {
     from_port        = 0
