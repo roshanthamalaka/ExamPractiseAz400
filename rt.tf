904e74a8f9ee8b10e6c6834ddbfc819268b8b385
roshanthamalaka <roshanthamalaka@gmail.com>
2022-12-01 22:07:33 +0530

    Route Table Added to the Feature Branch

---
+++ rt.tf
@@ -0,0 +1,14 @@
+#Create the Route Table 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    #gateway_id = aws_internet_gateway.gw.id
+  }
+   
+
+  tags = {
+    Name = "public"
+  }
+}
