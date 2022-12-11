cf7e40ebb82a1b4c169876c3e89cbf5006eac1a0
roshanthamalaka <roshanthamalaka@gmail.com>
2022-12-11 22:16:49 +0530

    Changes Commited to feature branch to demonstrate parallel merging

--- sg.tf
+++ sg.tf
@@ -31,6 +31,20 @@ resource "aws_security_group" "websg" {
     #ipv6_cidr_blocks = "0.0.0.0/0"
   }
 
+
+  #Added This Change to Feature Branch to Demonstrate Parallel branch change mering
+  ingress {
+    description      = "SMTP Allow"
+    from_port        = 25
+    to_port          = 25
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    #ipv6_cidr_blocks = "0.0.0.0/0"
+  }
+
+
+
+
   #Added this comment demonstrate go back to earlier version after commit No useful thing added 
   egress {
     from_port        = 0
@@ -40,6 +54,10 @@ resource "aws_security_group" "websg" {
     ipv6_cidr_blocks = ["::/0"]
   }
  
+
+
+
+
 
   tags = {
     Name = "websg"
